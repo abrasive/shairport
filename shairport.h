59d26d3a7ce00dcd9c6d5ef26953d161a622eab4
Dan McGee <dan@archlinux.org>
2012-02-24 16:14:38 -0600

    Use static where appropriate in shairport.c
    Most functions don't need to be public outside of this file, so move all
    the forward declarations out of shairport.h.

--- shairport.h
+++ shairport.h
@@ -52,33 +52,6 @@ struct connection
   char                *password;
 };
 
-RSA *loadKey();
-int startAvahi(const char *pHwAddr, const char *pServerName, int pPort);
-void cleanupBuffers(struct connection *pConnection);
-void cleanup(struct connection *pConnection);
-void handleClient(int pSock, char *pPassword, char *pHWADDR);
-int getAvailChars(struct shairbuffer *pBuf);
-
-char *getTrimmedMalloc(char *pChar, int pSize, int pEndStr, int pAddNL);
-char *getTrimmed(char *pChar, int pSize, int pEndStr, int pAddNL, char *pTrimDest);
-void initBuffer(struct shairbuffer *pBuf, int pNumChars);
-void printBufferInfo(struct shairbuffer *pBuf, int pLevel);
-void addToShairBuffer(struct shairbuffer *pBuf, char *pNewBuf);
-void addNToShairBuffer(struct shairbuffer *pBuf, char *pNewBuf, int pNofNewBuf);
-
-int readDataFromClient(int pSock, struct shairbuffer *pClientBuffer);
-int  parseMessage(struct connection *pConn,unsigned char *pIpBin, unsigned int pIpBinLen, char *pHWADDR);
-
-void closePipe(int *pPipe);
-void setKeys(struct keyring *pKeys, char *pIV, char* pAESKey, char *pFmtp);
-void initConnection(struct connection *pConn, struct keyring *pKeys, 
-                struct comms *pComms, int pSocket, char *pPassword);
-
-void writeDataToClient(int pSock, struct shairbuffer *pResponse);
-void propogateCSeq(struct connection *pConn);
-int buildAppleResponse(struct connection *pConn, unsigned char *pIpBin, unsigned int pIpBinLen, char *pHwAddr);
 void sim(int pLevel, char *pValue1, char *pValue2);
-void slog(int pLevel, char *pFormat, ...);
-int  isLogEnabledFor(int pLevel);
 
 #endif
