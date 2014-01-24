4f5af6f6c16655490d0c70dd9e69a4ee784905e0
James Laird <jhl@mafipulation.org>
2014-01-24 22:34:41 +1100

    fix RTSP fix
    Previous commit was broken due to my stupidity.

--- rtsp.c
+++ rtsp.c
@@ -413,8 +413,7 @@ static void handle_setup(rtsp_conn_info *conn,
     player_play(&conn->stream);
 
     char *resphdr = malloc(strlen(hdr) + 20);
-    strcpy(resphdr, hdr);
-    sprintf(resphdr + strlen(resphdr), ";server_port=%d;control_port=%d;timing_port=%d", sport, sport, sport);
+    sprintf(resphdr, "%s;server_port=%d;control_port=%d;timing_port=%d", "RTP/AVP/UDP;unicast;mode=record", sport, sport, sport);
     msg_add_header(resp, "Transport", resphdr);
 
     msg_add_header(resp, "Session", "1");
