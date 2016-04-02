c1118eedbfcb96a51c38fa1284767aca0e7a49c0
Mike Brady <mikebrady@eircom.net>
2016-04-02 15:08:17 +0000

    Reduce minimum buffer length below which no stuffin will be done.

--- player.c
+++ player.c
@@ -104,7 +104,7 @@ static pthread_mutex_t vol_mutex = PTHREAD_MUTEX_INITIALIZER;
 #define MAX_PACKET 2048
 
 // DAC buffer occupancy stuff
-#define DAC_BUFFER_QUEUE_MINIMUM_LENGTH 2000
+#define DAC_BUFFER_QUEUE_MINIMUM_LENGTH 600
 
 typedef struct audio_buffer_entry { // decoded audio packets
   int ready;
