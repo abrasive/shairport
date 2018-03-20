49a9cb9dfc2fa8021e6a647a633f37431d8e4fad
Mike Brady <mikebrady@eircom.net>
2018-03-20 17:00:50 +0000

    Remove the work "basic" from descriptions of the remote control.

--- dbus-service.c
+++ dbus-service.c
@@ -15,7 +15,7 @@
 #include "dbus-service.h"
 
 ShairportSyncDiagnostics *shairportSyncDiagnosticsSkeleton;
-ShairportSyncBasicRemoteControl *shairportSyncBasicRemoteControlSkeleton;
+ShairportSyncRemoteControl *shairportSyncRemoteControlSkeleton;
 
 void dbus_metadata_watcher(struct metadata_bundle *argc, __attribute__((unused)) void *userdata) {
   // debug(1, "DBUS metadata watcher called");
@@ -23,16 +23,16 @@ void dbus_metadata_watcher(struct metadata_bundle *argc, __attribute__((unused))
 
   // debug(1, "No diagnostics watcher required");
 
-  // debug(1, "DBUS basic remote control watcher called");
+  // debug(1, "DBUS remote control watcher called");
 
-  shairport_sync_basic_remote_control_set_airplay_volume(shairportSyncBasicRemoteControlSkeleton,
+  shairport_sync_remote_control_set_airplay_volume(shairportSyncRemoteControlSkeleton,
                                                          argc->airplay_volume);
 
   if (argc->dacp_server_active)
-    shairport_sync_basic_remote_control_set_server(shairportSyncBasicRemoteControlSkeleton,
+    shairport_sync_remote_control_set_server(shairportSyncRemoteControlSkeleton,
                                                    argc->client_ip);
   else
-    shairport_sync_basic_remote_control_set_server(shairportSyncBasicRemoteControlSkeleton, "");
+    shairport_sync_remote_control_set_server(shairportSyncRemoteControlSkeleton, "");
 
   GVariantBuilder *dict_builder, *aa;
 
@@ -98,110 +98,110 @@ void dbus_metadata_watcher(struct metadata_bundle *argc, __attribute__((unused))
   g_variant_builder_unref(dict_builder);
 
   // debug(1,"Set metadata");
-  shairport_sync_basic_remote_control_set_metadata(shairportSyncBasicRemoteControlSkeleton, dict);
+  shairport_sync_remote_control_set_metadata(shairportSyncRemoteControlSkeleton, dict);
 }
 
-static gboolean on_handle_fast_forward(ShairportSyncBasicRemoteControl *skeleton,
+static gboolean on_handle_fast_forward(ShairportSyncRemoteControl *skeleton,
                                        GDBusMethodInvocation *invocation,
                                        __attribute__((unused)) gpointer user_data) {
   send_simple_dacp_command("beginff");
-  shairport_sync_basic_remote_control_complete_fast_forward(skeleton, invocation);
+  shairport_sync_remote_control_complete_fast_forward(skeleton, invocation);
   return TRUE;
 }
 
-static gboolean on_handle_rewind(ShairportSyncBasicRemoteControl *skeleton,
+static gboolean on_handle_rewind(ShairportSyncRemoteControl *skeleton,
                                  GDBusMethodInvocation *invocation,
                                  __attribute__((unused)) gpointer user_data) {
   send_simple_dacp_command("beginrew");
-  shairport_sync_basic_remote_control_complete_rewind(skeleton, invocation);
+  shairport_sync_remote_control_complete_rewind(skeleton, invocation);
   return TRUE;
 }
 
-static gboolean on_handle_toggle_mute(ShairportSyncBasicRemoteControl *skeleton,
+static gboolean on_handle_toggle_mute(ShairportSyncRemoteControl *skeleton,
                                       GDBusMethodInvocation *invocation,
                                       __attribute__((unused)) gpointer user_data) {
   send_simple_dacp_command("mutetoggle");
-  shairport_sync_basic_remote_control_complete_toggle_mute(skeleton, invocation);
+  shairport_sync_remote_control_complete_toggle_mute(skeleton, invocation);
   return TRUE;
 }
 
-static gboolean on_handle_next(ShairportSyncBasicRemoteControl *skeleton,
+static gboolean on_handle_next(ShairportSyncRemoteControl *skeleton,
                                GDBusMethodInvocation *invocation,
                                __attribute__((unused)) gpointer user_data) {
   send_simple_dacp_command("nextitem");
-  shairport_sync_basic_remote_control_complete_next(skeleton, invocation);
+  shairport_sync_remote_control_complete_next(skeleton, invocation);
   return TRUE;
 }
 
-static gboolean on_handle_previous(ShairportSyncBasicRemoteControl *skeleton,
+static gboolean on_handle_previous(ShairportSyncRemoteControl *skeleton,
                                    GDBusMethodInvocation *invocation,
                                    __attribute__((unused)) gpointer user_data) {
   send_simple_dacp_command("previtem");
-  shairport_sync_basic_remote_control_complete_previous(skeleton, invocation);
+  shairport_sync_remote_control_complete_previous(skeleton, invocation);
   return TRUE;
 }
 
-static gboolean on_handle_pause(ShairportSyncBasicRemoteControl *skeleton,
+static gboolean on_handle_pause(ShairportSyncRemoteControl *skeleton,
                                 GDBusMethodInvocation *invocation,
                                 __attribute__((unused)) gpointer user_data) {
   send_simple_dacp_command("pause");
-  shairport_sync_basic_remote_control_complete_pause(skeleton, invocation);
+  shairport_sync_remote_control_complete_pause(skeleton, invocation);
   return TRUE;
 }
 
-static gboolean on_handle_play_pause(ShairportSyncBasicRemoteControl *skeleton,
+static gboolean on_handle_play_pause(ShairportSyncRemoteControl *skeleton,
                                      GDBusMethodInvocation *invocation,
                                      __attribute__((unused)) gpointer user_data) {
   send_simple_dacp_command("playpause");
-  shairport_sync_basic_remote_control_complete_play_pause(skeleton, invocation);
+  shairport_sync_remote_control_complete_play_pause(skeleton, invocation);
   return TRUE;
 }
 
-static gboolean on_handle_play(ShairportSyncBasicRemoteControl *skeleton,
+static gboolean on_handle_play(ShairportSyncRemoteControl *skeleton,
                                GDBusMethodInvocation *invocation,
                                __attribute__((unused)) gpointer user_data) {
   send_simple_dacp_command("play");
-  shairport_sync_basic_remote_control_complete_play(skeleton, invocation);
+  shairport_sync_remote_control_complete_play(skeleton, invocation);
   return TRUE;
 }
 
-static gboolean on_handle_stop(ShairportSyncBasicRemoteControl *skeleton,
+static gboolean on_handle_stop(ShairportSyncRemoteControl *skeleton,
                                GDBusMethodInvocation *invocation,
                                __attribute__((unused)) gpointer user_data) {
   send_simple_dacp_command("stop");
-  shairport_sync_basic_remote_control_complete_stop(skeleton, invocation);
+  shairport_sync_remote_control_complete_stop(skeleton, invocation);
   return TRUE;
 }
 
-static gboolean on_handle_resume(ShairportSyncBasicRemoteControl *skeleton,
+static gboolean on_handle_resume(ShairportSyncRemoteControl *skeleton,
                                  GDBusMethodInvocation *invocation,
                                  __attribute__((unused)) gpointer user_data) {
   send_simple_dacp_command("playresume");
-  shairport_sync_basic_remote_control_complete_resume(skeleton, invocation);
+  shairport_sync_remote_control_complete_resume(skeleton, invocation);
   return TRUE;
 }
 
-static gboolean on_handle_shuffle_songs(ShairportSyncBasicRemoteControl *skeleton,
+static gboolean on_handle_shuffle_songs(ShairportSyncRemoteControl *skeleton,
                                         GDBusMethodInvocation *invocation,
                                         __attribute__((unused)) gpointer user_data) {
   send_simple_dacp_command("shuffle_songs");
-  shairport_sync_basic_remote_control_complete_shuffle_songs(skeleton, invocation);
+  shairport_sync_remote_control_complete_shuffle_songs(skeleton, invocation);
   return TRUE;
 }
 
-static gboolean on_handle_volume_up(ShairportSyncBasicRemoteControl *skeleton,
+static gboolean on_handle_volume_up(ShairportSyncRemoteControl *skeleton,
                                     GDBusMethodInvocation *invocation,
                                     __attribute__((unused)) gpointer user_data) {
   send_simple_dacp_command("volumeup");
-  shairport_sync_basic_remote_control_complete_volume_up(skeleton, invocation);
+  shairport_sync_remote_control_complete_volume_up(skeleton, invocation);
   return TRUE;
 }
 
-static gboolean on_handle_volume_down(ShairportSyncBasicRemoteControl *skeleton,
+static gboolean on_handle_volume_down(ShairportSyncRemoteControl *skeleton,
                                       GDBusMethodInvocation *invocation,
                                       __attribute__((unused)) gpointer user_data) {
   send_simple_dacp_command("volumedown");
-  shairport_sync_basic_remote_control_complete_volume_down(skeleton, invocation);
+  shairport_sync_remote_control_complete_volume_down(skeleton, invocation);
   return TRUE;
 }
 
@@ -384,43 +384,43 @@ static void on_dbus_name_acquired(GDBusConnection *connection, const gchar *name
   g_signal_connect(shairportSyncDiagnosticsSkeleton, "notify::delta-time",
                    G_CALLBACK(notify_delta_time_callback), NULL);
 
-  // debug(1,"dbus_basic_remote_control_on_dbus_name_acquired");
-  shairportSyncBasicRemoteControlSkeleton = shairport_sync_basic_remote_control_skeleton_new();
+  // debug(1,"dbus_remote_control_on_dbus_name_acquired");
+  shairportSyncRemoteControlSkeleton = shairport_sync_remote_control_skeleton_new();
   g_dbus_interface_skeleton_export(
-      G_DBUS_INTERFACE_SKELETON(shairportSyncBasicRemoteControlSkeleton), connection,
+      G_DBUS_INTERFACE_SKELETON(shairportSyncRemoteControlSkeleton), connection,
       "/org/gnome/ShairportSync", NULL);
 
-  g_signal_connect(shairportSyncBasicRemoteControlSkeleton, "handle-fast-forward",
+  g_signal_connect(shairportSyncRemoteControlSkeleton, "handle-fast-forward",
                    G_CALLBACK(on_handle_fast_forward), NULL);
-  g_signal_connect(shairportSyncBasicRemoteControlSkeleton, "handle-rewind",
+  g_signal_connect(shairportSyncRemoteControlSkeleton, "handle-rewind",
                    G_CALLBACK(on_handle_rewind), NULL);
-  g_signal_connect(shairportSyncBasicRemoteControlSkeleton, "handle-toggle-mute",
+  g_signal_connect(shairportSyncRemoteControlSkeleton, "handle-toggle-mute",
                    G_CALLBACK(on_handle_toggle_mute), NULL);
-  g_signal_connect(shairportSyncBasicRemoteControlSkeleton, "handle-next",
+  g_signal_connect(shairportSyncRemoteControlSkeleton, "handle-next",
                    G_CALLBACK(on_handle_next), NULL);
-  g_signal_connect(shairportSyncBasicRemoteControlSkeleton, "handle-previous",
+  g_signal_connect(shairportSyncRemoteControlSkeleton, "handle-previous",
                    G_CALLBACK(on_handle_previous), NULL);
-  g_signal_connect(shairportSyncBasicRemoteControlSkeleton, "handle-pause",
+  g_signal_connect(shairportSyncRemoteControlSkeleton, "handle-pause",
                    G_CALLBACK(on_handle_pause), NULL);
-  g_signal_connect(shairportSyncBasicRemoteControlSkeleton, "handle-play-pause",
+  g_signal_connect(shairportSyncRemoteControlSkeleton, "handle-play-pause",
                    G_CALLBACK(on_handle_play_pause), NULL);
-  g_signal_connect(shairportSyncBasicRemoteControlSkeleton, "handle-play",
+  g_signal_connect(shairportSyncRemoteControlSkeleton, "handle-play",
                    G_CALLBACK(on_handle_play), NULL);
-  g_signal_connect(shairportSyncBasicRemoteControlSkeleton, "handle-stop",
+  g_signal_connect(shairportSyncRemoteControlSkeleton, "handle-stop",
                    G_CALLBACK(on_handle_stop), NULL);
-  g_signal_connect(shairportSyncBasicRemoteControlSkeleton, "handle-resume",
+  g_signal_connect(shairportSyncRemoteControlSkeleton, "handle-resume",
                    G_CALLBACK(on_handle_resume), NULL);
-  g_signal_connect(shairportSyncBasicRemoteControlSkeleton, "handle-shuffle-songs",
+  g_signal_connect(shairportSyncRemoteControlSkeleton, "handle-shuffle-songs",
                    G_CALLBACK(on_handle_shuffle_songs), NULL);
-  g_signal_connect(shairportSyncBasicRemoteControlSkeleton, "handle-volume-up",
+  g_signal_connect(shairportSyncRemoteControlSkeleton, "handle-volume-up",
                    G_CALLBACK(on_handle_volume_up), NULL);
-  g_signal_connect(shairportSyncBasicRemoteControlSkeleton, "handle-volume-down",
+  g_signal_connect(shairportSyncRemoteControlSkeleton, "handle-volume-down",
                    G_CALLBACK(on_handle_volume_down), NULL);
 
   add_metadata_watcher(dbus_metadata_watcher, NULL);
 
-#ifdef HAVE_DBUS_BASIC_REMOTE_CONTROL
-  dbus_basic_remote_control_on_dbus_name_acquired(connection, name, user_data);
+#ifdef HAVE_DBUS_REMOTE_CONTROL
+  dbus_remote_control_on_dbus_name_acquired(connection, name, user_data);
 #endif
 
   debug(1, "Shairport Sync native D-Bus service started at \"%s\" on the %s bus.", name,
