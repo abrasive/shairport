670c6fbea9f93e4c88ad1917670af354d49977a0
Mike Brady <mikebrady@eircom.net>
2018-04-06 08:22:38 +0100

    Check long term behahiour of scanner and interaction with periodic wakeups and bac scans. Unfinished.

--- metadata_hub.c
+++ metadata_hub.c
@@ -477,9 +477,9 @@ void metadata_hub_process_metadata(uint32_t type, uint32_t code, char *data, uin
       metadata_store.player_thread_active = 0;
       metadata_store.player_state = PS_STOPPED;
       // debug(1,"player_stop release track metadata and artwork");
-      metadata_hub_release_track_metadata(metadata_store.track_metadata);
-      metadata_store.track_metadata = NULL;
-      metadata_hub_release_track_artwork();
+      //metadata_hub_release_track_metadata(metadata_store.track_metadata);
+      //metadata_store.track_metadata = NULL;
+      //metadata_hub_release_track_artwork();
       metadata_hub_modify_epilog(1);
     } break;
     case 'pfls': {
--- shairport.c
+++ shairport.c
@@ -337,8 +337,8 @@ int parse_options(int argc, char **argv) {
   config.scan_interval_when_active = 1; // number of seconds between DACP server scans when playing something
   config.scan_interval_when_inactive = 3; // number of seconds between DACP server scans playing nothing
   config.scan_max_bad_response_count = 10; // number of successive bad results to ignore before giving up 
-//  config.scan_max_inactive_count = (15*60)/config.scan_interval_when_inactive; // number of scans to do before stopping if not made active again (15 minutes )
-  config.scan_max_inactive_count = 5; // number of scans to do before stopping if not made active again (15 minutes )
+  config.scan_max_inactive_count = (24*60*60)/config.scan_interval_when_inactive; // number of scans to do before stopping if not made active again (24 hours )
+//  config.scan_max_inactive_count = 5; // number of scans to do before stopping if not made active again (15 minutes )
 #endif
 
   // config_setting_t *setting;
